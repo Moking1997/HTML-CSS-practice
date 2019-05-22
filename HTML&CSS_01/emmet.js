c9eb5218101b2d61c1481537e442fd92c2609588
Moking1997 <Moking1997@outlook.com>
2019-05-22 14:23:46 +0800

    two practice
    two practice

---
+++ HTML&CSS_01/emmet.js
@@ -0,0 +1,38 @@
+Normalize.css使浏览器更加一致地呈现所有元素
+并符合现代标准。它只针对需要规范化的样式
+
+Activate Power Mode 打字特效
+emmet 插件快捷输入
+
+div*3
+<div></div>
+<div></div>
+<div></div>
+
+div{Yo}*3
+<div>Yo</div>
+<div>Yo</div>
+<div>Yo</div>
+
+div{Yo$}*3
+<div>Yo1</div>
+<div>Yo2</div>
+<div>Yo3</div>
+
+(div{Yo$}*3>p)
+<div>Yo1
+    <p></p>
+</div>
+<div>Yo2
+    <p></p>
+</div>
+<div>Yo3
+    <p></p>
+</div>
+
+.a
+#b
+span.c
+<div class="a"></div>
+<div id="b"></div>
+<span class="c"></span>
